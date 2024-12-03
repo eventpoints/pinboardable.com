dc4a6aa3d95e24c8805a8cf244480cadb866643a
kez <kerrialbeckettnewham@gmail.com>
2024-12-03 20:59:14 +0100

    add createdAt to reaction

--- src/Entity/Reaction.php
+++ src/Entity/Reaction.php
@@ -6,12 +6,14 @@ namespace App\Entity;
 
 use App\Enum\ReactionTypeEnum;
 use App\Repository\ReactionRepository;
+use Carbon\CarbonImmutable;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: ReactionRepository::class)]
-#[ORM\Index(name:"idx_pin_reaction_type", columns: ["pin_id", "reaction_type_enum"])]
+#[ORM\Index(name: "idx_pin_reaction_type", columns: ["pin_id", "reaction_type_enum"])]
 class Reaction
 {
     #[ORM\Id]
@@ -29,11 +31,15 @@ class Reaction
     #[ORM\ManyToOne(inversedBy: 'reactions')]
     private ?Pin $pin = null;
 
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
+    private null|CarbonImmutable $createdAt = null;
+
     public function __construct(?ReactionTypeEnum $reactionTypeEnum, ?string $fignerprint, ?Pin $pin = null)
     {
         $this->reactionTypeEnum = $reactionTypeEnum;
         $this->fignerprint = $fignerprint;
         $this->pin = $pin;
+        $this->createdAt = new CarbonImmutable();
     }
 
     public function getId(): Uuid|null
@@ -72,4 +78,15 @@ class Reaction
     {
         $this->fignerprint = $fignerprint;
     }
+
+    public function getCreatedAt(): ?CarbonImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?CarbonImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
 }
